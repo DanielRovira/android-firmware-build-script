0b20d51e9e91409044f8e7c2f26e3b671de8e783
DanielRovira <32240966+DanielRovira@users.noreply.github.com>
2022-06-20 22:12:49 -0300

    Delete rr directory

--- rr/extra (1).sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source config.sh
-rm -rf $HD/$ROM/hardware/qcom-caf/msm8996/audio
-rm -rf $HD/$ROM/hardware/qcom/audio
-git clone https://github.com/LineageOS/android_hawdware_qcom_audio -b lineage-17.1-caf-msm8996 $HD/$ROM/hardware/qcom-caf/msm8996/audio
-git clone https://github.com/LineageOS/android_hawdware_qcom_audio -b lineage-17.1 $HD/$ROM/hardware/qcom/audio
-cp -r -a $SCRIPT/$ROM/Android.bp $HD/$ROM/vendor/motorola/sdm632-common
